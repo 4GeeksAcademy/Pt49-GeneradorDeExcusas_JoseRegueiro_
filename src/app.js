a35df954a158a7f8c2acbda314b5d27e527cbf38
JoseRegueiro17 <132882721+JoseRegueiro17@users.noreply.github.com>
2023-10-06 19:13:19 +0000

    Agregando window.onload

--- src/app.js
+++ src/app.js
@@ -1,38 +1,46 @@
-let who = ["Mi perro", "Mi primo", "Mi gato", "Un Alien", "Mi cuñado"];
-let action = [
-  "se comió",
-  "me escondió",
-  "destrozó",
-  "se robó",
-  "manipuló mentalmente"
-];
-let what = [
-  "mi proyecto",
-  "mi codigo",
-  "mis llaves",
-  "mi cargador",
-  "mi portatil"
-];
-let when = [
-  "justo cuando iba a terminar.",
-  "al salir de casa.",
-  "al llegar al trabajo.",
-  "cuando ya estaba saliendo.",
-  "cuando estaba de vacaciones."
-];
-function bringRamdomElement(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
+import "bootstrap";
+import "./style.css";
 
-function excuseGenerator() {
-  return `${bringRamdomElement(who)} ${bringRamdomElement(
-    action
-  )} ${bringRamdomElement(what)} ${bringRamdomElement(when)}`;
-}
-let refreshPage = document.getElementById("refresh");
-refreshPage.addEventListener("click", _ => {
-  location.reload();
-});
-window.onload = excuseGenerator;
-const generador_excusa = document.getElementById("excusa");
-generador_excusa.textContent = excuseGenerator();
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  let who = ["Mi perro", "Mi primo", "Mi gato", "Un Alien", "Mi cuñado"];
+  let action = [
+    "se comió",
+    "me escondió",
+    "destrozó",
+    "se robó",
+    "manipuló mentalmente"
+  ];
+  let what = [
+    "mi proyecto",
+    "mi codigo",
+    "mis llaves",
+    "mi cargador",
+    "mi portatil"
+  ];
+  let when = [
+    "justo cuando iba a terminar.",
+    "al salir de casa.",
+    "al llegar al trabajo.",
+    "cuando ya estaba saliendo.",
+    "cuando estaba de vacaciones."
+  ];
+  function getRamdomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function generatorExcuse() {
+    return `${getRamdomElement(who)} ${getRamdomElement(
+      action
+    )} ${getRamdomElement(what)} ${getRamdomElement(when)}`;
+  }
+  let refreshPage = document.getElementById("refresh");
+  refreshPage.addEventListener("click", _ => {
+    location.reload();
+  });
+
+  const generador_excusa = document.getElementById("excusa");
+  generador_excusa.textContent = generatorExcuse();
+};
